8271133d729cf7030e47994ffc649f16a569ab09
Jihad Al Khurfan <jalkhurfan@gmail.com>
2021-06-23 19:58:08 +0300

    connected to postgres

--- src/main/java/com/jkh98/qna/dao/UserDataAccessService.java
+++ src/main/java/com/jkh98/qna/dao/UserDataAccessService.java
@@ -1,6 +1,9 @@
 package com.jkh98.qna.dao;
 
 import com.jkh98.qna.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -8,10 +11,20 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository("postgres")
-public class UserDataAccessService implements  UserDao{
+public class UserDataAccessService implements UserDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public UserDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public int insertUser(UUID id, User user) {
-        return 0;
+        String sql = "INSERT INTO person ( id, username )  VALUES (?, ?)";
+
+        return jdbcTemplate.update(sql, id, user.getUsername());
     }
 
     @Override
@@ -21,21 +34,38 @@ public class UserDataAccessService implements  UserDao{
 
     @Override
     public List<User> selectAllUsers() {
-        return null;
+        final String sql = "SELECT id, username FROM person";
+        return jdbcTemplate.query(sql, mapUserFomDb());
     }
 
     @Override
     public Optional<User> selectUserById(UUID id) {
-        return Optional.empty();
+        final String sql = "SELECT id, username FROM person WHERE id = ?";
+        List<User> users = jdbcTemplate.query(sql, mapUserFomDb(), id);
+
+        return Optional.ofNullable(users.isEmpty() ? null : users.get(0));
     }
 
     @Override
     public int deleteUserById(UUID id) {
-        return 0;
+        String sql = "DELETE FROM person WHERE id = ?";
+
+        return jdbcTemplate.update(sql, id);
     }
 
     @Override
     public int updateUserById(UUID id, User user) {
-        return 0;
+        String sql = "UPDATE person SET username = ? WHERE id = ?";
+
+        return jdbcTemplate.update(sql, user.getUsername(), id);
+    }
+
+    private RowMapper<User> mapUserFomDb() {
+        return ((resultSet, i) ->
+                new User(
+                        UUID.fromString(resultSet.getString("id")),
+                        resultSet.getString("username")
+                )
+        );
     }
 }
